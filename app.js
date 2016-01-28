c52c8c6167db84ce72d569d8d760e97785e59295
Scott Christensen <scottmc526@gmail.com>
2016-01-28 09:58:14 -0700

    require cookie-session and dotenv

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var cookieSession = require('cookie-session');
+var env = require('dotenv').load()
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
